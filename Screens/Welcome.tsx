b129c7fe332d27e28e02ed770c05ea8bf444fe70
augusto5959 <augustoviteri98@gmail.com>
2025-01-07 21:00:52 -0500

    Funcionamiento botones

--- Screens/Welcome.tsx
+++ Screens/Welcome.tsx
@@ -1,14 +1,31 @@
 import { Button, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
-export default function Welcome() {
-	return (
-		<View>
-			<Text>Welcome</Text>
-            <Button title='Login'/>
-            <Button title='Register'/>
-		</View>
-	);
+export default function Welcome({navigation}: any) { 
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Welcome</Text>
+            <Button 
+                title="Login" 
+				onPress={() => navigation.navigate('Login')}
+            />
+            <Button 
+                title="Register"
+				onPress={() => navigation.navigate('Register')} 
+            />
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
+    },
+    title: {
+        fontSize: 24,
+        marginBottom: 20,
+    },
+});

e519a663d23b4f1eb4d9d1f2167c9db57b7a6bc0
gabriel2354 <gabrielmoncayo93@gmail.com>
2025-01-07 22:09:06 -0500

    actualizacion

--- browsers/MainNavigator.tsx
+++ browsers/MainNavigator.tsx
@@ -3,6 +3,7 @@ import Welcome from '../Screens/Welcome';
 import LoginScreen from '../Screens/LoginScreen';
 import RegisterScreen from '../Screens/RegisterScreen';
 import { NavigationContainer } from '@react-navigation/native';
+import JuegoScreen from '../Screens/JuegoScreen';
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,7 @@ function MyStack(){
             <Stack.Screen name="Welcome" component={Welcome} />
             <Stack.Screen name="Login" component={LoginScreen} />
             <Stack.Screen name="Register" component={RegisterScreen} />
+            <Stack.Screen name="Juego" component={JuegoScreen} />
         </Stack.Navigator>
     )
 }
